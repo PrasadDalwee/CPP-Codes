a2dd89014562227b78f1d52f1658ad14b5505b8d
PrasadDalwee <prasaddalwee@gmail.com>
2021-12-20 10:00:58 +0530

    19 dec

--- 000-temp.cpp
+++ 000-temp.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 int main()
 {
-    stack <int> st;
-    st.push(1);
-
-    while(!st.empty())
-    {
-        cout<<st.top()<<" ";
-        st.pop();
-    }
-    cout<<st.top();
-    cout<<"exit";       //basically if we pop an empty stack program stops
+    
+    // if we pop an empty stack program stops
+    //a=b=c=3, assigns 3 to a,b,c;
+    // arr[n], arr is pointer to first element, *ptr prints value arr+1 will point to second element
+    // dynamic array of size n: int *arr=new int(n) |or| int *arr; arr=new int(n); creates arr[n]
+    
+    int *arr;
+    arr=new int(3);
+    arr[2]=555;
+    cout<<arr[2]<<" "<<*(arr+2);
 }
