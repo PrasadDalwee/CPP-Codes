d47bec0403820e57217f28d3362400e6562c51a6
PrasadDalwee <prasaddalwee@gmail.com>
2022-02-15 23:00:34 +0530

    feb15

--- 000-temp.cpp
+++ 000-temp.cpp
@@ -18,7 +18,9 @@ int main()
     //  4. dynamic array of size n: int *arr=new int(n) |or| int *arr; arr=new int(n); creates arr[n]
     //  5. use delete keyword to delete a pointer of some type defined(new LL()) from memory
     //  6. accessing right or left of NULL pointer stops program in Trees
-    
+    //  7. Inorder Traversal of BST always gives sorted order
+    //  8. Deleting a node in BST either replace node with its inorder predecessor(i.e. least or left most) in left subtree or inorder
+    //     successor(i.e. largest or right most) in right subtree
    int a=3;
    cout<<(a<<2)<<(a<<=2);
 }
